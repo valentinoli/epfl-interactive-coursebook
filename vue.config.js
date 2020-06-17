a4ef1f299802d5619feb6e5468bbee3429074c94
valentinoli <valentin.loftsson@gmail.com>
2020-06-17 23:58:29 +0200

    Add manifest properties, background_color and msTileColor white

--- vue.config.js
+++ vue.config.js
@@ -3,11 +3,21 @@ module.exports = {
   pwa: {
     name: "EPFL Interactive Coursebook",
     themeColor: "#ff0000",
+    msTileColor: "#ffffff",
+    assetsVersion: "1.0",
     manifestOptions: {
-      short_name: "Coursebook"
-    },
-    iconPaths: {
-      maskIcon: null
+      // https://developer.mozilla.org/en-US/docs/Web/Manifest
+      short_name: "Coursebook",
+      background_color: "#ffffff",
+      categories: ["education"],
+      description: "Interactive visualization of EPFL courses",
+      screenshots: [
+        {
+          src: "og-image.png",
+          sizes: "1200x630",
+          type: "image/png"
+        }
+      ]
     }
   }
 };
